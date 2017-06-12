2a8eea7bf071ba9ebc68fac08d082153f2b8b241
gshshaohui <shaohui@peekabuy.com>
2017-06-12 05:50:52 -0700

    web-rocket
    Great, make web app works!
    Work hard, Good luck!
    Rocket!

--- src/main/java/sjph/life/HelloWorld.java
+++ src/main/java/sjph/life/HelloWorld.java
@@ -1,7 +1,14 @@
-package com.sjph.life;
+package sjph.life;
 
+/**
+ * @author shaohuiguo
+ *
+ */
 public class HelloWorld {
 
+    /**
+     * @param args arguments
+     */
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("Work hard, Good luck!");
