4ac7b88e4d1c38e40bd454eb685fe9142053f293
gshshaohui <shaohui@peekabuy.com>
2017-06-11 23:44:22 -0700

    Work hard, Good luck

---
+++ src/main/java/com/sjph/life/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.sjph.life;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Work hard, Good luck!");
+    }
+
+}
