7fe11b0787159b907b4f31cd00e024c2cdc6c3eb
Shaohui Guo <sjph.guo@gmail.com>
2017-08-15 22:19:40 -0700

    check in new structure for Microservices:
    initial, more will come, firstly to support Spring Boot

---
+++ service-friendship/src/main/docker/run.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+echo "********************************************************"
+echo "Starting Life Friendship Server"
+echo "********************************************************"
+java -jar /usr/local/life/service_friendship/@project.build.finalName@.jar
