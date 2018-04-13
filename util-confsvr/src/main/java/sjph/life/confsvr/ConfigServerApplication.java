f87034b4df11ecbf0f43f7f087e713bd3dc56f1c
Shaohui Guo <sjph.guo@gmail.com>
2018-04-12 22:18:41 -0700

    [Design] fix pom's module name for confsvr, eureka, zuul and confsvr's compile issue, also remove rest&website folders

--- util-confsvr/src/main/java/sjph/life/confsvr/ConfigServerApplication.java
+++ util-confsvr/src/main/java/sjph/life/confsvr/ConfigServerApplication.java
@@ -1,4 +1,4 @@
-package sjph.life.service.confsvr;
+package sjph.life.confsvr;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
